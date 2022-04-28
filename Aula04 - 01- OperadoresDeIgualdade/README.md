e7ddd5cebf5c4700c7544ed22d0bfa9147738dc6
lucascagostinho <lucascagostinho@gmail.com>
2022-04-28 19:20:21 -0300

    Aula sobre operadores de igualdade

---
+++ Aula04 - 01- OperadoresDeIgualdade/README.md
@@ -0,0 +1,6 @@
+# Aprendendo sobre operadores de igualde
+
+## Tipos
+
+- == para comparar igualdaded (= é para atribuição) (== para comparação)
+- != para comparar desigualdade (! é para negação da igualdade)
---
+++ Aula04 - 01- OperadoresDeIgualdade/src/br/com/entra21/operadores/de/igualdade/Main.java
@@ -0,0 +1,29 @@
+package br.com.entra21.operadores.de.igualdade;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Operadores de igualdade");
+		
+		System.out.println("Igual ==");
+		
+		byte idadeA, idadeB;
+		boolean resultado;
+		
+		idadeA = 10;
+		idadeB = 10;
+		
+		resultado = idadeA == idadeB;
+		
+		System.out.println(resultado);
+		
+		resultado = idadeA != idadeB;
+		
+		System.out.println(resultado);
+		
+		
+		
+	}
+
+}
